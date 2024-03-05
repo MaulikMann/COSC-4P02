bd092961f484494a0715184fcf3f3bdee036b804
Brett Laptop <tri11paragon@tpgc.me>
2024-03-05 17:48:00 -0500

    remove extra words using stats

--- server/python_scripts/fetch.py
+++ server/python_scripts/fetch.py
@@ -3,6 +3,7 @@ import argparse
 import functools 
 import re
 from bs4 import BeautifulSoup
+import statistics
 
 def fetch_url(url):
     request = requests.get(url)
@@ -23,16 +24,65 @@ def fetch_url(url):
 def split_words(text):
     lines = (line.strip() for line in text.splitlines())
     
+    recombined_lines = []
+    charc_associations = []
+    wordc_associations = []
     for l in lines:
         if (len(l) != 0):
-            print(len(l))
+            charc_associations.append(len(l))
+            wc = len(re.split("\s", re.sub("[\"\']", "", l)))
+            wordc_associations.append(wc)
+            recombined_lines.append((l, wc))
+            
+    limit = 0
+    try:
+        wc_mean = statistics.mean(wordc_associations)
+        wc_stdev = statistics.stdev(wordc_associations)
+        wc_median = statistics.median(wordc_associations)
+        wc_variance = statistics.variance(wordc_associations, wc_mean)
+        wc_rv = wc_stdev / wc_mean
+        
+        cc_mean = statistics.mean(charc_associations)
+        cc_stdev = statistics.stdev(charc_associations)
+        cc_median = statistics.median(charc_associations)
+        cc_variance = statistics.variance(charc_associations, cc_mean)
+        cc_rv = cc_stdev / cc_mean
+        
+        # print(wc_mean)
+        # print(wc_stdev)
+        # print(wc_median)
+        # print(wc_variance)
+        # print(wc_rv)
+        
+        # print()
+
+        # print(cc_mean)
+        # print(cc_median)
+        # print(cc_stdev)
+        # print(cc_variance)
+        # print(cc_rv)
+        
+        limit = wc_mean
+        
+        if (wc_rv > 1):
+            limit += wc_stdev
+        else:
+            limit -= wc_stdev
+    except Exception as e:
+        print(e)
     
-    cleared_text = '\n'.join(line for line in lines if len(line) != 0)
+    if (limit < 10):
+        limit = 10
+        
+    cleared_text = '\n'.join(line[0] for line in recombined_lines if (line[1] > limit))
+
+    #print(cleared_text)
+    #exit(0)
     
     words = re.split("\s", re.sub("[\"\']", "", cleared_text))
     cleared_words = (word.strip() for word in words if word.strip())
     back_to_text = ' '.join(cleared_words)
-    return (list(cleared_words), back_to_text)
+    return (back_to_text)
 
 def word_count(words):
     return len(words)
--- server/python_scripts/socket_server.py
+++ server/python_scripts/socket_server.py
@@ -2,11 +2,18 @@
 
 import asyncio
 import websockets
+from summarizer import fetch_and_summarize
 
-
-async def handler(websocket):
-	async for message in websocket:
-		print(message)
+async def handler(websocket, path):
+	if path == "/s/summarizer":
+		async for prompt_url in websocket:
+			summary = fetch_and_summarize(prompt_url)
+			if (summary != null):
+				await websocket.send(summary)
+			else:
+				await websocket.send("error")
+	else:
+		await websocket.send("404: invalid endpoint")
 
 async def main():
     async with websockets.serve(handler, "", 42069):
