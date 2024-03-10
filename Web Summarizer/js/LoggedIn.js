b69741b2046db4e4550c4fcd5221a4757ffa231c
Maulik Mann <maulikmann@gmail.com>
2024-03-09 20:24:08 -0500

    log

---
+++ Web Summarizer/js/LoggedIn.js
@@ -0,0 +1,16 @@
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+
+const auth = getAuth();
+// Function to check if the user is signed in already
+export function checkIfUserSignedIn() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in
+      console.log("User is signed in:", user);
+      document.getElementById("greeting").innerText = `Hello, ${user.displayName || 'User'}`;
+    } else {
+      // No user is signed in
+      console.log("No user signed in");
+    }
+  });
+}
