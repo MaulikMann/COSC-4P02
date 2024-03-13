2d5e3096f7da308be5f42c0f41356526ae4d59a0
COSC4P0224 <cosc4p02@gmail.com>
2024-03-12 20:06:08 -0400

    test2

--- Web Summarizer/js/server.js
+++ Web Summarizer/js/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const shortid = require('shortid');
 const sqlite3 = require('sqlite3');
-//test
+//test hi
 const app = express();
 const PORT = 3000; 
 
