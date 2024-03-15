a9407627b0d04d4a2e63f4ce9927b1d0a441f6ee
Maulik Mann <maulikmann@gmail.com>
2024-03-15 14:45:10 -0400

    url

--- Web Summarizer/js/server.js
+++ Web Summarizer/js/server.js
@@ -80,7 +80,7 @@ app.get('/:shortCode', (req, res) => {
     });
 });
 
-app.get('/urls', (req, res) => {
+app.get('/u/urls', (req, res) => {
     db.all('SELECT * FROM urls', (err, rows) => {
         if (err) {
             console.error('Database error:', err);
