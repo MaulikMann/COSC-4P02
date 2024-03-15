bb7e7ce41955fa9b4d36a717599c35413c1fbdc3
Maulik Mann <maulikmann@gmail.com>
2024-03-15 18:09:01 -0400

    signout

--- Web Summarizer/js/signout.js
+++ Web Summarizer/js/signout.js
@@ -1,8 +1,11 @@
 import { signOut } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
 import {auth} from './firebase-init.js';
 
+const googleSignOut=document.getElementById("google-signout-btn");
+googleSignOut.addEventListener("click", function(){
 signOut(auth).then(() => {
   // Sign-out successful.
 }).catch((error) => {
   // An error happened.
 });
+})
