b8d9bfd38765e6dc920470927919f177115d4142
Maulik Mann <maulikmann@gmail.com>
2024-03-15 15:10:56 -0400

    url

--- Web Summarizer/js/db.js
+++ Web Summarizer/js/db.js
@@ -9,38 +9,42 @@ document.addEventListener("DOMContentLoaded", function() {
             // Create a Uint8Array from the array buffer
             var uInt8Array = new Uint8Array(arrayBuffer);
             
-            // Create the SQLite database
-            var db = new SQL.Database(uInt8Array);
-            
-            // Fetch data from the database and populate the table
-            var selectQuery = 'SELECT * FROM urls';
-            var results = db.exec(selectQuery);
+            try {
+                // Create the SQLite database
+                var db = new SQL.Database(uInt8Array);
+                
+                // Fetch data from the database and populate the table
+                var selectQuery = 'SELECT * FROM urls';
+                var results = db.exec(selectQuery);
 
-            var len = results[0].values.length;
-            var tableHead = document.getElementById('tableHead');
-            var tableBody = document.getElementById('tableBody');
+                var len = results[0].values.length;
+                var tableHead = document.getElementById('tableHead');
+                var tableBody = document.getElementById('tableBody');
 
-            // Generate table headers
-            var headerRow = document.createElement('tr');
-            headerRow.innerHTML = '<th>ID</th>' +
-                                  '<th>Short Code</th>' +
-                                  '<th>Long URL</th>' +
-                                  '<th>Click Count</th>' +
-                                  '<th>Created At</th>' +
-                                  '<th>User ID</th>';
-            tableHead.appendChild(headerRow);
+                // Generate table headers
+                var headerRow = document.createElement('tr');
+                headerRow.innerHTML = '<th>ID</th>' +
+                                      '<th>Short Code</th>' +
+                                      '<th>Long URL</th>' +
+                                      '<th>Click Count</th>' +
+                                      '<th>Created At</th>' +
+                                      '<th>User ID</th>';
+                tableHead.appendChild(headerRow);
 
-            // Generate table rows
-            for (var i = 0; i < len; i++) {
-                var row = results[0].values[i];
-                var newRow = document.createElement('tr');
-                newRow.innerHTML = '<td>' + row[0] + '</td>' +
-                                   '<td>' + row[1] + '</td>' +
-                                   '<td>' + row[2] + '</td>' +
-                                   '<td>' + row[3] + '</td>' +
-                                   '<td>' + row[4] + '</td>' +
-                                   '<td>' + row[5] + '</td>';
-                tableBody.appendChild(newRow);
+                // Generate table rows
+                for (var i = 0; i < len; i++) {
+                    var row = results[0].values[i];
+                    var newRow = document.createElement('tr');
+                    newRow.innerHTML = '<td>' + row[0] + '</td>' +
+                                       '<td>' + row[1] + '</td>' +
+                                       '<td>' + row[2] + '</td>' +
+                                       '<td>' + row[3] + '</td>' +
+                                       '<td>' + row[4] + '</td>' +
+                                       '<td>' + row[5] + '</td>';
+                    tableBody.appendChild(newRow);
+                }
+            } catch (error) {
+                console.error('Error creating database:', error);
             }
         })
         .catch(error => {
